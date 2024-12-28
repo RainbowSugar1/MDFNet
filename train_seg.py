bf6287be5703e72583ece111cca4869389b0fd8c
RainbowSugar1 <3177262360@qq.com>
2024-12-28 10:51:48 +0800

    add new files

---
+++ train_seg.py
@@ -0,0 +1,28 @@
+import sys
+import argparse
+import os
+
+sys.path.append(r'E:\MDFNet') # Path
+
+from ultralytics import YOLO
+
+def main(opt):
+    yaml = opt.cfg
+    model = YOLO(yaml)#.load('E:\daima/ultralyticsPro1020\yolov8n-seg.pt')
+
+    model.info()
+
+    # 实例分割训练
+    results = model.train(data='E:/MDFNet/ultralytics\cfg\datasets\coco128-seg.yaml',epochs=15,imgsz=640,  workers=8, batch=8)
+
+def parse_opt(known=False):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg', type=str, default= r'E:\MDFNet\ultralytics\cfg\models\cfg2024\YOLOv8-Seg\yyyy.yaml', help='initial weights path')
+    parser.add_argument('--weights', type=str, default='', help='')
+
+    opt = parser.parse_known_args()[0] if known else parser.parse_args()
+    return opt
+
+if __name__ == "__main__":
+    opt = parse_opt()
+    main(opt)
