73ebd215d5a32d5f8bf869c1decf35ebff8d1274
Wang Piao <3177262360@qq.com>
2025-02-26 15:46:51 +0800

    Delete predict.py

--- predict.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-#model = YOLO("yolov8n-seg.pt")  # load an official model
-model = YOLO("G:\实验结果\实验83/train19\weights/best.pt")  # load a custom model
-
-# Predict with the model
-results = model("E:\daima/ultralyticsPro1020\images/000089.jpg")  # predict on an image
-results[0].show()
